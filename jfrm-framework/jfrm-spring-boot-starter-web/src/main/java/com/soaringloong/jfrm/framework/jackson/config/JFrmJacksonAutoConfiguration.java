f4edb367f3e48a6401068096d3ae225820a9961c
kangtengjiao <317432023@qq.com>
2024-09-19 14:40:29 +0800

    所有jackson使用同一个objectMapper

--- jfrm-framework/jfrm-spring-boot-starter-web/src/main/java/com/soaringloong/jfrm/framework/jackson/config/JFrmJacksonAutoConfiguration.java
+++ jfrm-framework/jfrm-spring-boot-starter-web/src/main/java/com/soaringloong/jfrm/framework/jackson/config/JFrmJacksonAutoConfiguration.java
@@ -21,52 +21,52 @@ import java.util.List;
 @Slf4j
 public class JFrmJacksonAutoConfiguration {
 
-    @Value("${jfrm.web.serial-write-timestamp:false}")
-    private boolean serialWriteTimestamp = Boolean.FALSE;
+	@Value("${jfrm.web.serial-write-timestamp:false}")
+	private boolean serialWriteTimestamp = Boolean.FALSE;
 
-    private ObjectMapper objectMapper;
+	private ObjectMapper objectMapper;
 
-    /**
-     * ObjectMapper 提升至 Bean 级别才能对全局json序列化和反序列生效
-     *
-     * @return ObjectMapper
-     */
-    @Bean
-    public ObjectMapper objectMapper() {
-        this.objectMapper = JsonUtils.makeCustomObjectMapper(serialWriteTimestamp);
-        return objectMapper;
-    }
+	/**
+	 * ObjectMapper 提升至 Bean 级别才能对全局json序列化和反序列生效
+	 * @return ObjectMapper
+	 */
+	@Bean
+	public ObjectMapper objectMapper() {
+		this.objectMapper = JsonUtils.makeCustomObjectMapper(serialWriteTimestamp);
+		return objectMapper;
+	}
 
-    @Bean
-    @SuppressWarnings("InstantiationOfUtilityClass")
-    public JsonUtils jsonUtils() {
-        Assert.isTrue(this.objectMapper != null, "ObjectMapper must not be null.");
-        JsonUtils.init(this.objectMapper);
-        log.info("[init][初始化 JsonUtils 成功]");
-        return new JsonUtils();
-    }
+	@Bean
+	@SuppressWarnings("InstantiationOfUtilityClass")
+	public JsonUtils jsonUtils() {
+		Assert.isTrue(this.objectMapper != null, "ObjectMapper must not be null.");
+		JsonUtils.init(this.objectMapper);
+		log.info("[init][初始化 JsonUtils 成功]");
+		return new JsonUtils();
+	}
 
-    /**
-     * JackJson 序列化与反序列化配置
-     * <p>
-     * 使用此方法, 以下 spring-boot: jackson 全局日期格式化 配置 将会失效 详见
-     * JsonUtils.makeCustomObjectMapper() spring.jackson.time-zone=GMT+8
-     * spring.jackson.date-format=yyyy-MM-dd HH:mm:ss (这种配置仅针对java.util.Date生效) 原因: 会覆盖 @EnableAutoConfiguration 关于
-     * WebMvcAutoConfiguration 的配置
-     */
-    @Bean
-    public MappingJackson2HttpMessageConverter getMappingJackson2HttpMessageConverter(
-            List<ObjectMapper> objectMappers) {
-        objectMappers.forEach(e -> JsonUtils.makeCustomObjectMapper(e, serialWriteTimestamp));
-        MappingJackson2HttpMessageConverter mappingJackson2HttpMessageConverter = new MappingJackson2HttpMessageConverter();
-        mappingJackson2HttpMessageConverter.setObjectMapper(this.objectMapper != null ? this.objectMapper : CollUtil.getFirst(objectMappers));
+	/**
+	 * JackJson 序列化与反序列化配置
+	 * <p>
+	 * 使用此方法, 以下 spring-boot: jackson 全局日期格式化 配置 将会失效 详见
+	 * JsonUtils.makeCustomObjectMapper() spring.jackson.time-zone=GMT+8
+	 * spring.jackson.date-format=yyyy-MM-dd HH:mm:ss (这种配置仅针对java.util.Date生效) 原因:
+	 * 会覆盖 @EnableAutoConfiguration 关于 WebMvcAutoConfiguration 的配置
+	 */
+	@Bean
+	public MappingJackson2HttpMessageConverter getMappingJackson2HttpMessageConverter(
+			List<ObjectMapper> objectMappers) {
+		objectMappers.forEach(e -> JsonUtils.makeCustomObjectMapper(e, serialWriteTimestamp));
+		MappingJackson2HttpMessageConverter mappingJackson2HttpMessageConverter = new MappingJackson2HttpMessageConverter();
+		mappingJackson2HttpMessageConverter
+			.setObjectMapper(this.objectMapper != null ? this.objectMapper : CollUtil.getFirst(objectMappers));
 
-        // 设置中文编码格式
-        List<MediaType> mediaTypes = new ArrayList<>();
-        mediaTypes.add(MediaType.APPLICATION_JSON);
-        mediaTypes.add(MediaType.TEXT_HTML);
-        mappingJackson2HttpMessageConverter.setSupportedMediaTypes(mediaTypes);
-        return mappingJackson2HttpMessageConverter;
-    }
+		// 设置中文编码格式
+		List<MediaType> mediaTypes = new ArrayList<>();
+		mediaTypes.add(MediaType.APPLICATION_JSON);
+		mediaTypes.add(MediaType.TEXT_HTML);
+		mappingJackson2HttpMessageConverter.setSupportedMediaTypes(mediaTypes);
+		return mappingJackson2HttpMessageConverter;
+	}
 
 }
