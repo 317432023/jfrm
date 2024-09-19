d53dc072a237355a33034d6a266139758963e041
kangtengjiao <317432023@qq.com>
2024-09-19 15:16:25 +0800

    加载com.frm.springmvcGlobalExceptionHandler

--- jfrm-server/src/main/java/com/soaringloong/jfrm/server/JFrmApplication.java
+++ jfrm-server/src/main/java/com/soaringloong/jfrm/server/JFrmApplication.java
@@ -3,8 +3,8 @@ package com.soaringloong.jfrm.server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(
-		scanBasePackages = { "${jfrm.info.base-package}.server", "${jfrm.info.base-package}.module", "com.cmpt.oss" })
+@SpringBootApplication(scanBasePackages = { "${jfrm.info.base-package}.server", "${jfrm.info.base-package}.module",
+		"com.cmpt.oss", /*加载com.frm.springmvcGlobalExceptionHandler*/"com.frm.springmvc" })
 public class JFrmApplication {
 
 	public static void main(String[] args) {
