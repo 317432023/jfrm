75a5995543e4ea4162100e32dde6e7dfcd91c60e
kangtengjiao <317432023@qq.com>
2024-09-19 23:12:46 +0800

    #

--- jfrm-framework/jfrm-spring-boot-starter-web/src/main/java/com/soaringloong/jfrm/framework/swagger/config/JFrmSwaggerAutoConfiguration.java
+++ jfrm-framework/jfrm-spring-boot-starter-web/src/main/java/com/soaringloong/jfrm/framework/swagger/config/JFrmSwaggerAutoConfiguration.java
@@ -35,113 +35,112 @@ import java.util.Optional;
  * 2. Swagger 规范，于 2015 更名为 OpenAPI 规范，本质是一个东西
  */
 @AutoConfiguration
-@ConditionalOnClass({OpenAPI.class})
+@ConditionalOnClass({ OpenAPI.class })
 @EnableConfigurationProperties(SwaggerProperties.class)
-@ConditionalOnProperty(prefix = "springdoc.api-docs", name = "enabled", havingValue = "true", matchIfMissing = true/*显式设置为 false 时，禁用*/)
+@ConditionalOnProperty(prefix = "springdoc.api-docs", name = "enabled", havingValue = "true",
+		matchIfMissing = true/* 显式设置为 false 时，禁用 */)
 public class JFrmSwaggerAutoConfiguration {
 
-    // ========== 全局 OpenAPI 配置 ==========
+	// ========== 全局 OpenAPI 配置 ==========
 
-    @Bean
-    public OpenAPI createApi(SwaggerProperties properties) {
-        Map<String, SecurityScheme> securitySchemas = buildSecuritySchemes();
-        OpenAPI openAPI = new OpenAPI()
-                // 接口信息
-                .info(buildInfo(properties))
-                // 接口安全配置
-                .components(new Components().securitySchemes(securitySchemas));
-        securitySchemas.keySet().forEach(key -> openAPI.addSecurityItem(new SecurityRequirement().addList(key)));
-        return openAPI;
-    }
+	@Bean
+	public OpenAPI createApi(SwaggerProperties properties) {
+		Map<String, SecurityScheme> securitySchemas = buildSecuritySchemes();
+		OpenAPI openAPI = new OpenAPI()
+			// 接口信息
+			.info(buildInfo(properties))
+			// 接口安全配置
+			.components(new Components().securitySchemes(securitySchemas));
+		securitySchemas.keySet().forEach(key -> openAPI.addSecurityItem(new SecurityRequirement().addList(key)));
+		return openAPI;
+	}
 
-    /**
-     * API 摘要信息
-     */
-    private Info buildInfo(SwaggerProperties properties) {
-        return new Info().title(properties.getTitle())
-                .description(properties.getDescription())
-                .version(properties.getVersion())
-                .contact(new Contact().name(properties.getAuthor()).url(properties.getUrl()).email(properties.getEmail()))
-                .license(new License().name(properties.getLicense()).url(properties.getLicenseUrl()));
-    }
+	/**
+	 * API 摘要信息
+	 */
+	private Info buildInfo(SwaggerProperties properties) {
+		return new Info().title(properties.getTitle())
+			.description(properties.getDescription())
+			.version(properties.getVersion())
+			.contact(new Contact().name(properties.getAuthor()).url(properties.getUrl()).email(properties.getEmail()))
+			.license(new License().name(properties.getLicense()).url(properties.getLicenseUrl()));
+	}
 
-    /**
-     * 安全模式，这里配置通过请求头 Authorization 传递 token 参数
-     */
-    private Map<String, SecurityScheme> buildSecuritySchemes() {
-        Map<String, SecurityScheme> securitySchemes = new HashMap<>();
-        SecurityScheme securityScheme = new SecurityScheme().type(SecurityScheme.Type.APIKEY) // 类型
-                .name(HttpHeaders.AUTHORIZATION) // 请求头的 name
-                .in(SecurityScheme.In.HEADER); // token 所在位置
-        securitySchemes.put(HttpHeaders.AUTHORIZATION, securityScheme);
-        return securitySchemes;
-    }
+	/**
+	 * 安全模式，这里配置通过请求头 Authorization 传递 token 参数
+	 */
+	private Map<String, SecurityScheme> buildSecuritySchemes() {
+		Map<String, SecurityScheme> securitySchemes = new HashMap<>();
+		SecurityScheme securityScheme = new SecurityScheme().type(SecurityScheme.Type.APIKEY) // 类型
+			.name(HttpHeaders.AUTHORIZATION) // 请求头的 name
+			.in(SecurityScheme.In.HEADER); // token 所在位置
+		securitySchemes.put(HttpHeaders.AUTHORIZATION, securityScheme);
+		return securitySchemes;
+	}
 
-    /**
-     * 自定义 OpenAPI 处理器
-     */
-    @Bean
-    @Primary // 目的：以我们创建的 OpenAPIService Bean 为主，避免一键改包后，启动报错！
-    public OpenAPIService openApiBuilder(Optional<OpenAPI> openAPI, SecurityService securityParser,
-                                         SpringDocConfigProperties springDocConfigProperties, PropertyResolverUtils propertyResolverUtils,
-                                         Optional<List<OpenApiBuilderCustomizer>> openApiBuilderCustomizers,
-                                         Optional<List<ServerBaseUrlCustomizer>> serverBaseUrlCustomizers,
-                                         Optional<JavadocProvider> javadocProvider) {
-        return new OpenAPIService(openAPI, securityParser, springDocConfigProperties, propertyResolverUtils,
-                openApiBuilderCustomizers, serverBaseUrlCustomizers, javadocProvider);
-    }
+	/**
+	 * 自定义 OpenAPI 处理器
+	 */
+	@Bean
+	@Primary // 目的：以我们创建的 OpenAPIService Bean 为主，避免一键改包后，启动报错！
+	public OpenAPIService openApiBuilder(Optional<OpenAPI> openAPI, SecurityService securityParser,
+			SpringDocConfigProperties springDocConfigProperties, PropertyResolverUtils propertyResolverUtils,
+			Optional<List<OpenApiBuilderCustomizer>> openApiBuilderCustomizers,
+			Optional<List<ServerBaseUrlCustomizer>> serverBaseUrlCustomizers,
+			Optional<JavadocProvider> javadocProvider) {
+		return new OpenAPIService(openAPI, securityParser, springDocConfigProperties, propertyResolverUtils,
+				openApiBuilderCustomizers, serverBaseUrlCustomizers, javadocProvider);
+	}
 
-    // ========== 分组 OpenAPI 配置 ==========
+	// ========== 分组 OpenAPI 配置 ==========
 
-    /**
-     * 所有模块的 API 分组
-     */
-    @Bean
-    public GroupedOpenApi allGroupedOpenApi() {
-        return buildGroupedOpenApi("all", "");
-    }
+	/**
+	 * 所有模块的 API 分组
+	 */
+	@Bean
+	public GroupedOpenApi allGroupedOpenApi() {
+		return buildGroupedOpenApi("all", "");
+	}
 
-    public static GroupedOpenApi buildGroupedOpenApi(String group) {
-        return buildGroupedOpenApi(group, group);
-    }
+	public static GroupedOpenApi buildGroupedOpenApi(String group) {
+		return buildGroupedOpenApi(group, group);
+	}
 
-    public static GroupedOpenApi buildGroupedOpenApi(String group, String path) {
-        return GroupedOpenApi.builder()
-                .group(group)
-                .pathsToMatch("/admin-api/" + path + "/**", "/app-api/" + path + "/**")
-                .addOperationCustomizer(
-                        (operation, handlerMethod) -> operation.addParametersItem(buildTenantHeaderParameter())
-                                .addParametersItem(buildSecurityHeaderParameter()))
-                .build();
-    }
+	public static GroupedOpenApi buildGroupedOpenApi(String group, String path) {
+		return GroupedOpenApi.builder()
+			.group(group)
+			.pathsToMatch("/admin-api/" + path + "/**", "/app-api/" + path + "/**")
+			.addOperationCustomizer(
+					(operation, handlerMethod) -> operation.addParametersItem(buildTenantHeaderParameter())
+						.addParametersItem(buildSecurityHeaderParameter()))
+			.build();
+	}
 
-    /**
-     * 构建 Tenant 租户编号请求头参数
-     *
-     * @return 多租户参数
-     */
-    private static Parameter buildTenantHeaderParameter() {
-        return new Parameter().name(WebFrameworkUtils.HEADER_TENANT_ID) // header 名
-                .description("租户编号") // 描述
-                .in(String.valueOf(SecurityScheme.In.HEADER)) // 请求 header
-                .schema(new IntegerSchema()._default(1L).name(WebFrameworkUtils.HEADER_TENANT_ID).description("租户编号")); // 默认：使用租户编号为1
-    }
+	/**
+	 * 构建 Tenant 租户编号请求头参数
+	 * @return 多租户参数
+	 */
+	private static Parameter buildTenantHeaderParameter() {
+		return new Parameter().name(WebFrameworkUtils.HEADER_TENANT_ID) // header 名
+			.description("租户编号") // 描述
+			.in(String.valueOf(SecurityScheme.In.HEADER)) // 请求 header
+			.schema(new IntegerSchema()._default(1L).name(WebFrameworkUtils.HEADER_TENANT_ID).description("租户编号")); // 默认：使用租户编号为1
+	}
 
-    /**
-     * 构建 Authorization 认证请求头参数
-     * <p>
-     * 解决 Knife4j <a href="https://gitee.com/xiaoym/knife4j/issues/I69QBU">Authorize
-     * 未生效，请求header里未包含参数</a>
-     *
-     * @return 认证参数
-     */
-    private static Parameter buildSecurityHeaderParameter() {
-        return new Parameter().name(HttpHeaders.AUTHORIZATION) // header 名
-                .description("认证 Token") // 描述
-                .in(String.valueOf(SecurityScheme.In.HEADER)) // 请求 header
-                .schema(new StringSchema()._default("Bearer test1")
-                        .name(WebFrameworkUtils.HEADER_TENANT_ID)
-                        .description("认证 Token")); // 默认：使用用户编号为 1
-    }
+	/**
+	 * 构建 Authorization 认证请求头参数
+	 * <p>
+	 * 解决 Knife4j <a href="https://gitee.com/xiaoym/knife4j/issues/I69QBU">Authorize
+	 * 未生效，请求header里未包含参数</a>
+	 * @return 认证参数
+	 */
+	private static Parameter buildSecurityHeaderParameter() {
+		return new Parameter().name(HttpHeaders.AUTHORIZATION) // header 名
+			.description("认证 Token") // 描述
+			.in(String.valueOf(SecurityScheme.In.HEADER)) // 请求 header
+			.schema(new StringSchema()._default("Bearer test1")
+				.name(WebFrameworkUtils.HEADER_TENANT_ID)
+				.description("认证 Token")); // 默认：使用用户编号为 1
+	}
 
 }
